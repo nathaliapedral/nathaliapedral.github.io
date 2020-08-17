80f4597dc3bfa2ae8bb7f17b1aa21de3de7b4385
Nathália Pedral <nathaliapedrall@gmail.com>
2020-08-16 23:00:28 -0300

    Building the game board and creating click event

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,6 @@
 const BACK = "card-back";
 const FRONT = "card-front";
+const CARD = "card"
 
 let techs = [
     'bootstrap',
@@ -16,6 +17,54 @@ let techs = [
 
 let cards = null;
 startGame();
+
+function startGame() {
+    cards = createCardsFromTechs(techs);
+    shuffleCards(cards);
+    initializeCards(cards);
+}
+
+function initializeCards(cards) {
+
+    let board = document.getElementById("board");
+
+    cards.forEach(card => {
+        let cardElement = document.createElement("div");
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+
+        board.appendChild(cardElement);
+    })
+}
+
+function createCardContent(card, cardElement) {
+
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function flipCard() {
+    this.classList.add("flip");
+}
+
+
+function createCardFace(cardFaceClass, card, parentElement) {
+    let cardFace = document.createElement("div");
+    cardFace.classList.add(cardFaceClass);
+    if (cardFaceClass === FRONT) {
+        let iconImg = document.createElement("img");
+        iconImg.src = "./assets/images/" + card.icon + ".png";
+        cardFace.appendChild(iconImg);
+    } else {
+        cardFace.innerHTML = "&lt/&gt";
+    }
+    parentElement.appendChild(cardFace);
+}
 
 function createCardsFromTechs(techs) {
 
@@ -49,11 +98,7 @@ function createRandomId(tech) {
     return tech + parseInt(Math.random() * 1000);
 }
 
-function startGame() {
-    cards = createCardsFromTechs(techs);
-    shuffleCards(cards);
-    console.log(cards);
-}
+
 
 function shuffleCards(cards) {
     let currentIndex = cards.length;
